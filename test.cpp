2e3a2202b7a4eb9990fa02f1c0746c3967c651e1
Matthew Clark <mclark4386@gmail.com>
2016-11-06 14:55:30 -0600

    *moved things around a bit to better orginize them

--- test.cpp
+++ test.cpp
@@ -3,7 +3,9 @@
 
 using namespace std;
 
-void print_packet(WebSocketPacket& packet);
+string print_bool(bool _b){
+  return _b ? "true" : "false";
+}
 
 int main(int argc, char *argv[])
 {
@@ -35,17 +37,7 @@ int main(int argc, char *argv[])
   cout << "just fin&RSV(1)"<<endl;
   print_packet(testFinAndRSV);
 
+  cout<< "Is valid from client packet:"<<print_bool(validFromClientPacket(testFinAndRSV))<<endl;
+  
   return 0;
-}
-
-void print_packet(WebSocketPacket& packet){
-  cout<<"WebSocketPacket{"<<endl;
-  cout<<"\tfin:\t"<<(packet.fin == true);
-  cout<<"\trsv:\t"<<int(packet.rsv);
-  cout<<"\topcode:\t"<<int(packet.opcode);
-  cout<<"\tmask_bit:\t"<<(packet.mask_bit == true);
-  cout<<"\tpayload_length:\t"<<packet.payload_length;
-  cout<<"\tmake_key:\t"<<packet.mask_key[0]<<packet.mask_key[1]<<packet.mask_key[2]<<packet.mask_key[3]<<endl;
-  cout<<"\tdecoded_payload:\t"<<string(packet.decoded_payload, packet.payload_length)<<endl;
-  cout<<"}"<<endl;
 }
--- websocket_helpers.hpp
+++ websocket_helpers.hpp
@@ -1,52 +1,20 @@
-#include <cstdint>
+#include <iostream>
+#include "websocket_packet.hpp"
 
-struct WebSocketPacket
-{
-  const char* raw;
-  char fin; //is bool, but most systems will have bool be bigger than char.... so just treat it like a bool
-  char rsv;
-  char opcode;
-  char mask_bit;//is bool
-  uint64_t payload_length;
-  char mask_key[4];
-  char* decoded_payload;
+using namespace std;
 
-  WebSocketPacket(const char* _raw):raw(_raw){
-    int offset = 0;
-    char eine_char = this->raw[offset];
-    this->fin = ((eine_char & 0b10000000) == 0b10000000);
-    this->rsv = ((eine_char & 0b01110000) >> 4);
-    this->opcode = (eine_char & 0b00001111);
+void print_packet(WebSocketPacket& packet){
+  cout<<"WebSocketPacket{"<<endl;
+  cout<<"\tfin:\t"<<(packet.fin == true);
+  cout<<"\trsv:\t"<<int(packet.rsv);
+  cout<<"\topcode:\t"<<int(packet.opcode);
+  cout<<"\tmask_bit:\t"<<(packet.mask_bit == true);
+  cout<<"\tpayload_length:\t"<<packet.payload_length;
+  cout<<"\tmake_key:\t"<<packet.mask_key[0]<<packet.mask_key[1]<<packet.mask_key[2]<<packet.mask_key[3]<<endl;
+  cout<<"\tdecoded_payload:\t"<<string(packet.decoded_payload, packet.payload_length)<<endl;
+  cout<<"}"<<endl;
+}
 
-    //move to the next byte
-    eine_char = this->raw[++offset];
-    this->mask_bit = ((eine_char & 0b10000000) == 0b10000000);
-    this->payload_length = (eine_char & 0b01111111);
-    if(this->payload_length == 126){//get next 16bits
-      this->payload_length = (int(this->raw[offset+1]) << 8) + int(this->raw[offset+2]);
-      offset +=2;
-    }else if(this->payload_length == 127){//get next 64bits
-      this->payload_length = (uint64_t(this->raw[offset+1]) << 24) +(uint64_t(this->raw[offset+2]) << 16) +(uint64_t(this->raw[offset+3]) << 8) + uint64_t(this->raw[offset+4]);
-      offset += 4;
-    }
-
-    //payload_length should not have the actual length in it
-    this->decoded_payload = new char[this->payload_length];
-    
-    if(this->mask_bit){
-      //read mask_key
-      for(int i=0;i<4;i++) { this->mask_key[i] = this->raw[++offset]; }
-      //demask
-      offset++;
-      for(int i=0; i < this->payload_length; i++){
-	this->decoded_payload[i] = this->raw[offset+i] ^ this->mask_key[i % 4];
-      }
-    }else{
-      //copy
-      offset++;
-      for(int i=0; i < this->payload_length; i++){
-	this->decoded_payload[i] = this->raw[offset+i];
-      }
-    }
-  }
-};
+bool validFromClientPacket(WebSocketPacket& packet){
+  return (packet.mask_bit == true);
+}
